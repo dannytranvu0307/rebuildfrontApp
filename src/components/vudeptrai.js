42c7621fd596c072edf836a2784728861e945f5d
tran vu <quachvotinh006@gmail.com>
2022-09-24 12:20:31 +0900

    vu da them file

---
+++ src/components/vudeptrai.js
@@ -0,0 +1,41 @@
+
+
+
+function vudeptrai(){
+    return (
+        <section className="bg-gray-50 mt-[-50px]">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            
+            <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 0">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Sign in to your account
+                    </h1>
+                    <div className="space-y-4 md:space-y-6">
+                        <div>
+                            <label className ="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
+                            <input type="email"  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" />
+                        </div>
+                        <div>
+                            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password"  placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " />
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <div className="flex items-start">
+        
+                            <a  className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                        </div>
+                        <button type="submit" className="w-full m-5 text-gray-900 bg-gray-100 hover:bg-blue-200 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Sign in</button>
+                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Don’t have an account yet? <a  className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+      </section>
+    )
+}
+
+export default vudeptrai;
